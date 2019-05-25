414343392d33bbb68ce2c5af79a44a1fa38ddd84
ezio84 <brabus84@gmail.com>
2019-05-25 17:29:36 +0700

    Add Google Sound Search tile

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -49,6 +49,7 @@ import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.ThemeTile;
+import com.android.systemui.qs.tiles.SoundSearchTIle;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.ScreenrecordTile;
@@ -140,6 +141,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new GamingModeTile(mHost);
             case "screenrecord":
                 return new ScreenrecordTile(mHost);
+			case "soundsearch":
+                return new SoundSearchTIle(mHost);
 
         }
 
