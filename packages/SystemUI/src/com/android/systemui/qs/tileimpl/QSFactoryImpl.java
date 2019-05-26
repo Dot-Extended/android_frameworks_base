355ed58b7c30fb9a199d6cec49d3de5d297e4f4d
romanbb <romanbirg@gmail.com>
2019-05-26 16:54:09 +0700

    Add Compass tile
    - Port Pie - SuperDroidBond
    - Port to Oreo - LorDClockAN
    
    Change-Id: I55532f5225efc76bd0217450b80bd8b9fb31de0c

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -33,6 +33,7 @@ import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.CaffeineTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
+import com.android.systemui.qs.tiles.CompassTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
@@ -143,6 +144,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new ScreenrecordTile(mHost);
 			case "soundsearch":
                 return new SoundSearchTIle(mHost);
+            case "compass":
+                return new CompassTile(mHost);
 
         }
 
